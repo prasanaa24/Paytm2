fa42367263749cc5174b5dd4b453026d9721422d
Gayathri.muthukumar <Gayathri.muthukumar@GSR-LAP33.GSRCHENNAI.LOCAL>
2017-01-23 15:22:33 +0530

    Gayathri Locators

---
+++ Paytm/src/main/java/Model/LoginModel.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class LoginModel {
+ 
+		
+	//Homepage Locators//
+	//
+		public static String PayTmLogoByXPath = "//a[@class='_2Y-B']/i";
+		public static String HamburgerMenuByXpath = "//i[@class='iconMenu cursor']";
+		public static String FreemovieByXpath = "//a[@title ='Free Movies']/img";
+		public static String HomepagesearchByXpath = "//div[@class=’_3HxD’]/input";
+		public static String MobileAccessoriesByXpath = "//div[@class='Ij1g']/a[2]";
+		public static String AndroidphonesByXpath = "//a[contains(text(),'Android Phones')]";
+		
+		//Android mobile phones page locators//
+		public static String FirstphoneByXpath = "//div[@class=’_3nWP’]/img";
+		public static String BuyoptionByXpath = "//div[@class='buy-bar']/button[1]";
+		public static String wishlisticonByXpath = "//i[@class='fa fa-heart-o']";
+		
+		
+				
+				
+	
+				
+				
+				
+		
+
+	
+
+}
